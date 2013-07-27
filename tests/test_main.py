d566834cb7d6230a0e701c2b5576e5db13e8b592
Audrey Roy <audreyr@gmail.com>
2013-07-27 14:50:14 +0200

    Stub main test.

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_main
+------------
+
+Tests for `alotofeffort.main` module.
+"""
+
+import unittest
+
+class TestMain(unittest.TestCase):
+
+    def test_something(self):
+        pass
