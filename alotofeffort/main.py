cd1eec6e945e1e26dbc0b39c44aefd9ff92e2284
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2015-09-11 14:05:34 -0700

    Fixes for issues found by pep8.

--- alotofeffort/main.py
+++ alotofeffort/main.py
@@ -8,10 +8,10 @@ from .send import deploy
 
 def main():
     """ Entry point for the package, as defined in setup.py. """
-    
+
     # Log info and above to console
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-    
+
     # Get command line input/output arguments
     parser = argparse.ArgumentParser(
         description='Instantly deploy static HTML sites to S3 at the command line.'
@@ -25,7 +25,7 @@ def main():
         help='Name of S3 bucket to deploy to, e.g. mybucket.'
     )
     args = parser.parse_args()
-    
+
     # Deploy the site to S3!
     deploy(args.www_dir, args.bucket_name)
 
--- alotofeffort/send.py
+++ alotofeffort/send.py
@@ -11,6 +11,7 @@ from boto.s3.key import Key
 
 logger = logging.getLogger(__name__)
 
+
 def deploy_file(file_path, bucket):
     """ Uploads a file to an S3 bucket, as a public file. """
 
@@ -21,7 +22,7 @@ def deploy_file(file_path, bucket):
     logger.info("Deploying {0}".format(file_path))
 
     # Upload the actual file to file_path
-    k = Key(bucket)    
+    k = Key(bucket)
     k.key = file_path
     try:
         k.set_contents_from_filename(file_path)
@@ -30,7 +31,7 @@ def deploy_file(file_path, bucket):
         logger.warning("Caught socket.error while trying to upload {0}".format(file_path))
         logger.warning("Please file an issue with alotofeffort if you see this,")
         logger.warning("providing as much info as you can.")
-    
+
 
 def deploy(www_dir, bucket_name):
     """ Deploy to the configured S3 bucket. """
@@ -45,7 +46,7 @@ def deploy(www_dir, bucket_name):
         for f in files:
             # Use full relative path. Normalize to remove dot.
             file_path = os.path.normpath(os.path.join(root, f))
-            
+
             if has_changed_since_last_deploy(file_path, bucket):
                 deploy_file(file_path, bucket)
             else:
@@ -53,10 +54,10 @@ def deploy(www_dir, bucket_name):
 
     # Make the whole bucket public
     bucket.set_acl('public-read')
-    
+
     # Configure it to be a website
     bucket.configure_website('index.html', 'error.html')
-    
+
     # Print the endpoint, so you know the URL
     logger.info("Your website is now live at {0}".format(bucket.get_website_endpoint()))
     logger.info("If you haven't done so yet, point your domain name there!")
@@ -65,22 +66,22 @@ def deploy(www_dir, bucket_name):
 def has_changed_since_last_deploy(file_path, bucket):
     """
     Checks if a file has changed since the last time it was deployed.
-    
+
     :param file_path: Path to file which should be checked. Should be relative
                       from root of bucket.
     :param bucket_name: Name of S3 bucket to check against.
     :returns: True if the file has changed, else False.
     """
-    
+
     logger.debug("Checking if {0} has changed since last deploy.".format(file_path))
     with open(file_path) as f:
         data = f.read()
         file_md5 = hashlib.md5(data.encode('utf-8')).hexdigest()
         logger.debug("file_md5 is {0}".format(file_md5))
-    
+
     key = bucket.get_key(file_path)
 
-    # HACK: Boto's md5 property does not work when the file hasn't been 
+    # HACK: Boto's md5 property does not work when the file hasn't been
     # downloaded. The etag works but will break for multi-part uploaded files.
     # http://stackoverflow.com/questions/16872679/how-to-programmatically-
     #     get-the-md5-checksum-of-amazon-s3-file-using-boto/17607096#17607096
